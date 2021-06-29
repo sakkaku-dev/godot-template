d3d3b134008403ebea5ab64a3ddc266417a2c285
myin <minyin142p@gmail.com>
2021-06-29 20:30:20 +0200

    Specify export file name

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,21 @@ if [ -z $PLATFORM ]; then
     exit
 fi
 
+
+# Needed until using version with https://github.com/godotengine/godot/pull/43621
+FILE=$PLATFORM
+if [ $PLATFORM == "web" ]; then
+    FILE="index.html"
+elif [ $PLATFORM == "win" ]; then
+    FILE="main.exe"
+elif [ $PLATFORM == "linux" ]; then
+    FILE="main.x86_64"
+elif [ $PLATFORM == "mac" ]; then
+    FILE="mac.zip"
+fi
+
 mkdir -v -p build/$PLATFORM
-godot -v --export "$PLATFORM" build/$PLATFORM/$PLATFORM
+godot -v --export "$PLATFORM" build/$PLATFORM/$FILE
 
 if [ "$PLATFORM" != "mac" ]; then
     cd build/$PLATFORM && zip -r $PLATFORM.zip *
