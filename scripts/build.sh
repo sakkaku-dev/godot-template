e48e09cc29a45595fa727e48716e324bdce6c3b4
myin <minyin142p@gmail.com>
2021-06-29 20:54:55 +0200

    Use = to compare

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,13 @@ fi
 
 # Needed until using version with https://github.com/godotengine/godot/pull/43621
 FILE=$PLATFORM
-if [ $PLATFORM -eq 'web' ]; then
+if [ $PLATFORM = 'web' ]; then
     FILE="index.html"
-elif [ $PLATFORM -eq 'win' ]; then
+elif [ $PLATFORM = 'win' ]; then
     FILE="main.exe"
-elif [ $PLATFORM -eq 'linux' ]; then
+elif [ $PLATFORM = 'linux' ]; then
     FILE="main.x86_64"
-elif [ $PLATFORM -eq 'mac' ]; then
+elif [ $PLATFORM = 'mac' ]; then
     FILE="mac.zip"
 fi
 
