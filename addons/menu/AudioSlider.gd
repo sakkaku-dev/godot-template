cb9aeb9c0b07a595da7db8d5b1b72739d4bd8491
myin <minyin142p@gmail.com>
2021-07-02 16:32:28 +0200

    Add volume slider

---
+++ addons/menu/AudioSlider.gd
@@ -0,0 +1,26 @@
+extends HSlider
+
+var master_id
+var vol_range = 60
+var vol_offset = 5
+
+func _ready():
+	master_id = AudioServer.get_bus_index("Master")
+	connect("value_changed", self, "_volume_changed")
+	value = get_volume_percentage()
+
+func _volume_changed(v: float):
+	if v == 0:
+		AudioServer.set_bus_mute(master_id, true)
+	else:
+		AudioServer.set_bus_mute(master_id, false)
+		AudioServer.set_bus_volume_db(master_id, get_volume(v))
+
+func get_volume(percentage: float) -> float:
+	var vol_value = -vol_range + (vol_range * percentage / 100) # Volume from -vol_range to 0
+	vol_value += vol_offset
+	return vol_value
+
+func get_volume_percentage():
+	var volume = AudioServer.get_bus_volume_db(master_id)
+	return (volume - vol_offset + vol_range) * 100 / vol_range # Just reversed the equation of #get_volume
--- addons/menu/Menu.gd
+++ addons/menu/Menu.gd
@@ -2,18 +2,25 @@ extends Control
 
 class_name Menu
 
+export var init_focus_path: NodePath
+onready var init_focus := get_node(init_focus_path) if init_focus_path else null
+
 export var back_menu_path: NodePath
 onready var back_menu := get_node(back_menu_path) if back_menu_path else null
-
 
 
 func show():
 	.show()
 	
-	if get_child_count() > 0:
+	if init_focus:
+		_focus_node(init_focus)
+	elif get_child_count() > 0:
 		var child = get_child(0)
-		child.show()
-		child.grab_focus()
+		_focus_node(child)
+
+func _focus_node(node: Control) -> void:
+	node.show()
+	node.grab_focus()
 
 func go_back():
 	if not back_menu: return
