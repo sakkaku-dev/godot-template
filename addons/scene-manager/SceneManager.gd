889003a6063876072f0f4e52e2f3c2160af7786c
myin <minyin142p@gmail.com>
2023-01-22 11:11:59 +0100

    fix scene manager + reload scene function

--- addons/scene-manager/SceneManager.gd
+++ addons/scene-manager/SceneManager.gd
@@ -14,15 +14,25 @@ func _input(event):
 	if transitioning:
 		get_tree().set_input_as_handled()
 
-func change_scene(scene: PackedScene):
+
+func reload_scene():
+	change_scene(get_tree().current_scene.filename)
+
+
+func change_scene(scene):
+	var path = scene
+	if scene is PackedScene:
+		path = scene.resource_path
+	print(path)
 	transitioning = true
 	
 	yield(_play_transition(DEFAULT_SPEED, false), "completed")
-	get_tree().change_scene_to(scene)
+	get_tree().change_scene(path)
 	yield(_play_transition(DEFAULT_SPEED, true), "completed")
 
 	transitioning = false
 	emit_signal("scene_changed")
+
 
 func _play_transition(speed: float, backwards: bool):
 	anim.playback_speed = speed
