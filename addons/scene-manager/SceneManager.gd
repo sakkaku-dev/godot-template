b40268c90633ddd94daba4ade32b557adaf968b1
Min Yin <minyin142p@gmail.com>
2023-02-03 21:51:51 +0100

    update scene manager to godot 4

--- addons/scene-manager/SceneManager.gd
+++ addons/scene-manager/SceneManager.gd
@@ -20,15 +20,15 @@ func reload_scene():
 
 
 func change_scene(scene):
-	var path = scene
-	if scene is PackedScene:
-		path = scene.resource_path
 	transitioning = true
-	
 	await _play_transition(DEFAULT_SPEED, false)
-	get_tree().change_scene(path)
+	
+	if scene is PackedScene:
+		get_tree().change_scene_to_packed(scene)
+	else:
+		get_tree().change_scene_to_file(scene)
+		
 	await _play_transition(DEFAULT_SPEED, true)
-
 	transitioning = false
 	emit_signal("scene_changed")
 
