8cd4f9eb08cd624f264330cd44747b1591b08128
myin <minyin142p@gmail.com>
2023-02-06 18:10:27 +0100

    customize transition + speed in scene manager

--- addons/scene-manager/SceneManager.gd
+++ addons/scene-manager/SceneManager.gd
@@ -2,11 +2,40 @@ extends Node
 
 signal scene_changed()
 
+enum Transition {
+	FULL,
+	CIRCLE,
+	CURTAIN_H,
+	CURTAIN_V,
+	DIAGONAL,
+	DIAGONAL_SQUARE,
+	HORIZONTAL,
+	VERTICAL,
+	SCRIBBLES,
+	RADIAL,
+}
+
+const ROOT_DIR = "res://addons/scene-manager"
+const SHADER_DIR = ROOT_DIR + "/shader_patterns"
+const TRANSITION_MAP = {
+	Transition.FULL: "full.png",
+	Transition.CIRCLE: "circle.png",
+	Transition.CURTAIN_H: "curtains_h.png",
+	Transition.CURTAIN_V: "curtains.png",
+	Transition.DIAGONAL: "diagonal.png",
+	Transition.DIAGONAL_SQUARE: "squares.png",
+	Transition.HORIZONTAL: "horizontal.png",
+	Transition.VERTICAL: "vertical.png",
+	Transition.SCRIBBLES: "scribbles.png",
+	Transition.RADIAL: "radial.png",
+}
+
 const DEFAULT_SPEED = 2
 
 @export var disable_inputs = true
 
 @onready var anim := $AnimationPlayer
+@onready var rect: ColorRect = $CanvasLayer/ColorRect
 
 var transitioning = false
 
@@ -19,24 +48,38 @@ func reload_scene():
 	change_scene(get_tree().current_scene.filename)
 
 
-func change_scene(scene):
-	transitioning = true
-	await _play_transition(DEFAULT_SPEED, false)
-	
+func change_scene(scene, transition = Transition.FULL, speed = DEFAULT_SPEED):
+	var path = scene
 	if scene is PackedScene:
-		get_tree().change_scene_to_packed(scene)
-	else:
-		get_tree().change_scene_to_file(scene)
-		
-	await _play_transition(DEFAULT_SPEED, true)
+		path = scene.resource_path
+	transitioning = true
+	
+	await _fade(false, transition, speed)
+	get_tree().change_scene_to_file(path)
+	await _fade(true, transition, speed)
+
 	transitioning = false
 	emit_signal("scene_changed")
 
 
 func _play_transition(speed: float, backwards: bool):
-	anim.playback_speed = speed
+	anim.speed_scale = speed
 	if backwards:
 		anim.play_backwards("Transition")
 	else:
 		anim.play("Transition")
 	await anim.animation_finished
+
+
+func _fade(reverse: bool, transition: Transition, speed: float):
+	var eff = Effect.new()
+	var start = 0.0 if reverse else 1.0
+	var end = 1.0 if reverse else 0.0
+	eff.setup_props("shader_parameter/cutoff", start, end)
+	var mat: ShaderMaterial = rect.get_material()
+	mat.set("shader_parameter/mask", load(SHADER_DIR + "/" + TRANSITION_MAP[transition]))
+	eff.obj = mat
+	
+	eff.duration = speed
+	add_child(eff)
+	await eff.finished
