796b311992f333e11bb8e8be4fdb7b59fdecb797
myin <minyin142p@gmail.com>
2023-05-22 15:46:31 +0200

    update init template to run multiple times

--- scripts/init-template.sh
+++ scripts/init-template.sh
@@ -6,8 +6,11 @@ if [ -z $GAME_NAME ]; then
 fi
 
 echo "Setting game name to $GAME_NAME"
-sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" project.godot
-sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
-sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" scripts/publish.sh
+sed -e "/config\/name/ s/\".*\"/\"$GAME_NAME\"/" -i project.godot
+sed -e "/GAME=/ s/\".*\"/\"$GAME_NAME\"/" -i scripts/publish.sh
+
+# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" project.godot
+# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
+# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" scripts/publish.sh
 
 echo "#$GAME_NAME" > README.md
