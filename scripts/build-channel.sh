2867e70812663407e4daa1770752b0ed7ef84c84
myin <minyin142p@gmail.com>
2023-07-08 13:31:49 +0200

    fix build script

--- scripts/build-channel.sh
+++ scripts/build-channel.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 CHANNEL=${CHANNEL:-$1}
-VERSION=${$2:-dev}
 
-# mkdir ~/.config
-# cp /root/.config/godot ~/.config -r
 cd godot
 mkdir -v -p build/$CHANNEL
-godot --export $CHANNEL
+godot --export-release $CHANNEL
