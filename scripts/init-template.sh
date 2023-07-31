d46c9418502dd3236d6d1b6bc0036d865d452162
myin <minyin142p@gmail.com>
2023-07-31 17:17:24 +0200

    update release workflow with changelog

--- scripts/init-template.sh
+++ scripts/init-template.sh
@@ -8,9 +8,7 @@ fi
 echo "Setting game name to $GAME_NAME"
 sed -e "/config\/name/ s/\".*\"/\"$GAME_NAME\"/" -i godot/project.godot
 sed -e "/GAME=/ s/\".*\"/\"$GAME_NAME\"/" -i scripts/publish.sh
-
-# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" project.godot
-# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
-# sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" scripts/publish.sh
+sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
+sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" package.json
 
 echo "#$GAME_NAME" > README.md
