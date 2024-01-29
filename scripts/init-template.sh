4a102fcb0edd0a4192f9cd91f83d42a991bf8134
myin <minyin142p@gmail.com>
2024-01-29 20:51:01 +0100

    fix init template

--- scripts/init-template.sh
+++ scripts/init-template.sh
@@ -12,4 +12,4 @@ sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
 sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" package.json
 
 echo "# $GAME_NAME" > README.md
-git update-index --assume-unchanged src/env/Build.gd
+git update-index --assume-unchanged godot/src/env/Build.gd
