80e25e7b025be6d2d6c2ff50882aeb76942b3e1f
myin <minyin142p@gmail.com>
2024-02-17 18:58:30 +0100

    Update build-channel.sh

--- scripts/build-channel.sh
+++ scripts/build-channel.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 CHANNEL=${CHANNEL:-$1}
+DEBUG=$2
 
 cd godot
 mkdir -v -p ../build/$CHANNEL
-godot --export-release --headless $CHANNEL
+
+if [[ $DEBUG -eq 1 ]]; then 
+    godot --export-debug --headless $CHANNEL
+else
+    godot --export-release --headless $CHANNEL
+fi
